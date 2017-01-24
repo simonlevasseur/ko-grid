40945e0371ee3de59e9eb94344a9f751f39f557e
Simon LeVasseur <slevasseur@macadamian.com>
2017-01-24 02:26:07 -0500

    Display data in basic example

--- src/bindings/nssg-tbody.js
+++ src/bindings/nssg-tbody.js
@@ -1,14 +1,13 @@
 ko.bindingHandlers.nssgTbody = {
     init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
         var gridVM = ko.unwrap(bindingContext.$component);
-        var itemsOnCurrentPage = gridVM.pager.itemsOnCurrentPage;
 
         /************************/
         /**     DATA BINDING   **/
         /************************/
         ko.applyBindingsToNode(element, {
             foreach: {
-                data: itemsOnCurrentPage,
+                data: gridVM.data().data,
                 as: 'row'
             }
         }, bindingContext);
