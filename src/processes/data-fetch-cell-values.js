2745372a9f3dc03888d0bf3cd15897d0101df6cb
Jake Tober <jake@macadamian.com>
2017-02-10 12:03:54 -0500

    Resizing columns should not flag the data as having been modified

--- src/processes/data-fetch-cell-values.js
+++ src/processes/data-fetch-cell-values.js
@@ -6,6 +6,22 @@
 gridState.processors['data-fetch-cell-values'] = {
     watches: ['data', 'columns'],
     runs: function (options) {
+        
+        //Check to make sure this is a change worth updating for
+        options.cache.dataAccessors = options.cache.dataAccessors || {};
+        var columnsInOrder = JSON.stringify(options.model.columns.map(function(col){return col.id}));
+        var accessorsDiffer = options.model.columns.some(function(col){
+            return col.dataAccessor !== options.cache.dataAccessors[col.id];
+        });
+        if (!options.changed.data && !accessorsDiffer && options.cache.lastColumns === columnsInOrder){
+            //if the data is the same and the accessors or list of columns didn't change, then don't modify anything
+            return;
+        }
+        options.cache.lastColumns = columnsInOrder;
+        options.model.columns.forEach(function(col){
+            options.cache.dataAccessors[col.id] = col.dataAccessor;
+        });
+        
         if (options.model.logging) {
             console.log('Fetching cell values');
         }
