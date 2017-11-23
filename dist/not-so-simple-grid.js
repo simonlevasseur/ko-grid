d0909dad2e34b502c9860af8b0667a8a6b1f3340
Henry Yi <henry@macadamian.com>
2017-11-23 13:52:01 -0500

    Ensure currentPage is an int

--- dist/not-so-simple-grid.js
+++ dist/not-so-simple-grid.js
@@ -2762,12 +2762,18 @@ templates["text-th"] = "<div class=\"nssg-th-text\" data-bind=\"text: col.headin
         this.firstItem = propertyAsObservable(gridVM.paging, 'firstItem');
         this.lastItem = propertyAsObservable(gridVM.paging, 'lastItem');
         this.totalItems = propertyAsObservable(gridVM.paging, 'totalItems');
-        this.currentPageIndex = ko.pureComputed({ read: function () {
-            return gridVM.paging().currentPage;
-        },
+        this.currentPageIndex = ko.pureComputed({
+            read: function () {
+                return gridVM.paging().currentPage;
+            },
             write: function (newValue) {
-                gridVM.process({ paging: { currentPage: newValue } });
-            } });
+                var page = parseInt(newValue);
+                if (isNaN(page)) {
+                    page = 1;
+                }
+                gridVM.process({ paging: { currentPage: page } });
+            }
+        });
         this.pageSize = ko.pureComputed({ read: function () {
             return gridVM.paging().pageSize;
         },
